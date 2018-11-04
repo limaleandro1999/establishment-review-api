af19c93cf56ae7d6fb7bb1dedc2939294aa030cf
limaleandro1999 <limaleandro1999@gmail.com>
2018-11-04 10:27:17 -0300

    setting up for deploy on heroku

--- src/common/environment.ts
+++ src/common/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     db: {url: process.env.DB_URL || 'mongodb://localhost/establishment-review-api'},
-    server: {port: process.env.SERVER_PORT || 3000}
+    server: {port: process.env.PORT || 3000}
 }
